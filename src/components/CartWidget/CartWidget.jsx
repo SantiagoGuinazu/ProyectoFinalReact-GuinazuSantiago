03d3c3efc598b09b3a0a8a7ca2ae8fe320730a83
Santiago Guinazu <santigui2003@gmail.com>
2023-09-13 20:48:49 -0300

    carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -7,7 +7,7 @@ const CartWidget = () => {
     const {cartQuantity} = useContext(CartContext)
     return (
         <div className='d-flex justify-content-around align-items-center'>
-            <BsCart4 fontSize={'3.5rem'} color='white'/>
+            <BsCart4 style={{ width: '10rem', margin: '0.5rem' }} fontSize={'3.5rem'} color='white'/>
             {cartQuantity() > 0 && <Badge bg='danger'>{cartQuantity()}</Badge>}
         </div>    
     );
